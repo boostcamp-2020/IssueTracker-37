216a105842213442cce0d21789507ce452552978
hoo00nn <hoo00nn@naver.com>
2020-10-28 18:46:08 +0900

    Style #noissue :  double quote -> single quote 변경

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Index from "./pages/index";
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Index from './pages/index';
 
 const App = () => {
   return (
