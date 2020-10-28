331a3447d3279dbbb786841b4f1567f59018173f
hoo00nn <hoo00nn@naver.com>
2020-10-28 18:32:02 +0900

    Chore #37 : webpack path resolve 등록

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,57 +1,63 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const mode = process.env.NODE_ENV || "development";
+const mode = process.env.NODE_ENV || 'development';
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
+    filename: 'bundle.js',
     path: path.resolve(`${__dirname}/build`),
+  },
+  resolve: {
+    alias: {
+      '@util': path.join(__dirname, 'src/utils'),
+      '@lib': path.join(__dirname, 'src/lib'),
+    },
   },
   mode,
   devServer: {
-    contentBase: path.resolve("./build"),
-    index: "index.html",
+    contentBase: path.resolve('./build'),
+    index: 'index.html',
     historyApiFallback: true,
     port: 3000,
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: "/node_modules",
-        use: ["babel-loader"],
+        exclude: '/node_modules',
+        use: ['babel-loader'],
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader",
+            loader: 'html-loader',
             options: { minimize: true },
           },
         ],
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "index.html",
+      template: './public/index.html',
+      filename: 'index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: "style.css",
+      filename: 'style.css',
     }),
     new CleanWebpackPlugin(),
   ],
