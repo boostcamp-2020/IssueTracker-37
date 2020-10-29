6dc5f1e023d9208e32bb422cd4a6dd6b80518c2c
Wonseok Song <dnjstjr0517@gmail.com>
2020-10-29 20:59:25 +0900

    Feat #32 : 회원가입 API 구현
    - routes, controller, service 계층구조 사용
    - 회원가입시 패스워드 bcrypt로 암호화

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from 'express';
 
+import user from './user/user-routes';
+
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
   return res.json({ result: 'true' });
 });
+
+router.use('/api/user', user);
 
 export default router;
