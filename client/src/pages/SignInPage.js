02c4cd14288075b95063e1f2c045ddaa39503a38
kimhakjun <dkd7142@naver.com>
2020-10-29 15:22:00 +0900

    Feat #31 : 로그인 페이지 라우팅
    - pages 폴더에 SignInPage.js 작성
    - App.js 에서 SignInPage 라우팅 추가

---
+++ client/src/pages/SignInPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SignInPage = () => {
+  return (
+    <>
+        로그인 페이지 입니다.
+    </>
+  );
+};
+
+export default SignInPage;
