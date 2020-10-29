e8d0bbc0e19b3a15a4040f40f40581c19072f36c
kimhakjun <dkd7142@naver.com>
2020-10-29 21:08:28 +0900

    Feat #27 : ID 유효성검사 구현
    - 정규표현식이용하여 email 유효성체크 구현

--- client/src/utils/validator.js
+++ client/src/utils/validator.js
@@ -1,8 +1,10 @@
 const regex = {
-    password : /^[a-zA-Z0-9]{6,12}$/
-} 
+  email: /^[\w]([-_.]?[\w])*@[\w]([-_.]?[\w])*\.[a-zA-Z]{2,3}/i,
+  password: /^[a-zA-Z0-9]{6,12}$/,
+};
 
+const isPassword = (testPassword) => {
+  return regex.password.test(testPassword);
+};
 
-export const isPassword = (testPassword) => {
-    return regex.password.test(testPassword);
-}
+export default isPassword;
