a8ef1625c53ab82271783d323f1d3f9e56d7331a
Wonseok Song <dnjstjr0517@gmail.com>
2020-10-30 10:35:20 +0900

    Style #noissue : 유저라우터 중복된거 삭제
    - Merge 후 중복된 부분 삭제

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,8 +1,6 @@
 import express, { Request, Response } from 'express';
 import userRouter from './user/user-routes';
 
-import user from './user/user-routes';
-
 const router = express.Router();
 
 router.use('/user', userRouter);
@@ -10,7 +8,5 @@ router.use('/user', userRouter);
 router.get('/', (req: Request, res: Response) => {
   return res.json({ result: 'true' });
 });
-
-router.use('/user', user);
 
 export default router;
