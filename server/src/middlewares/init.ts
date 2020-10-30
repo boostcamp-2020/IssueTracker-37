61e5eb9c604aeb26452fcfa9dd6f62067b17502e
hoo00nn <hoo00nn@naver.com>
2020-10-30 11:28:41 +0900

    Feat #noissue : cors 설정 추가
    - 다른 port에서 api 요청 허용하도록 설정함

--- server/src/middlewares/init.ts
+++ server/src/middlewares/init.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import logger from 'morgan';
 import dotenv from 'dotenv';
 import apiRouter from '../api/index';
@@ -11,6 +12,7 @@ const initMiddlewares = (app: express.Application) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
+  app.use(cors({ origin: process.env.ORIGIN, credentials: true }));
   app.use('/api', apiRouter);
 };
 
