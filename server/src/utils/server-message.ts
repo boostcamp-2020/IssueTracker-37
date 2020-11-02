e2a54cdee3354abc93df0f5b2b8ebae76b2529da
hoo00nn <hoo00nn@naver.com>
2020-11-02 16:49:19 +0900

    Feat #noissue : server-message 추가
    - error 메시지 추가

--- server/src/utils/server-message.ts
+++ server/src/utils/server-message.ts
@@ -4,12 +4,14 @@ interface errorMsg {
   invalidToken: string;
   failedInsert: string;
   failedUpdate: string;
-  faildDelete: string;
-  failSelect: string;
   duplicatedUser: string;
+  failedDelete: string;
+  failedSelect: string;
+  failedRegister: string;
 }
 
 interface succeedMsg {
+  succedLogin: string;
   succeedInsert: string;
   succeedUpdate: string;
   succeedDelete: string;
@@ -23,12 +25,14 @@ const errorMessage: errorMsg = {
   invalidToken: `유효하지 않은 토큰입니다.`,
   failedInsert: `데이터 추가에 실패하였습니다.`,
   failedUpdate: `데이터 업데이트에 실패하였습니다.`,
-  faildDelete: `데이터 삭제에 실패하였습니다.`,
-  failSelect: `데이터를 정상적으로 가져오는데 실패하였습니다.`,
+  failedDelete: `데이터 삭제에 실패하였습니다.`,
+  failedSelect: `데이터를 정상적으로 가져오는데 실패하였습니다.`,
+  failedRegister: `회원가입에 실패했습니다.`,
   duplicatedUser: `이미 존재하는 아이디 입니다.`,
 };
 
 const succeedMessage: succeedMsg = {
+  succedLogin: `로그인이 정상적으로 완료되었습니다.`,
   succeedInsert: `데이터 추가가 정상적으로 완료되었습니다.`,
   succeedUpdate: `데이터 업데이트가 정상적으로 완료되었습니다.`,
   succeedDelete: `데이터 삭제가 정상적으로 완료되었습니다.`,
