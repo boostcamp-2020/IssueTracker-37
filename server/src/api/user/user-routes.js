de2441b7d60afd966660606862e6d849bdfb4b09
sumniy <sumniy94@gmail.com>
2020-11-03 12:54:07 +0900

    Feat #23 : GitHub Callback 라우트, 컨트롤러 구현
    - GitHub 인증 후 Callback 처리 컨트롤러 함수 구현
    - 토큰은 쿠키로 설정하고 클라이언트측에서 Localstorage로 저장하도록
      구현예정

--- server/src/api/user/user-routes.js
+++ server/src/api/user/user-routes.js
@@ -1,9 +1,18 @@
 const express = require('express');
 
 const userController = require('@api/user/user-controller');
+const passport = require('@passport');
 
 const router = express.Router();
 
 router.post('/signup', userController.signup);
+
+router.get('/github', passport.authenticate('github'));
+
+router.get(
+  '/github/callback',
+  passport.authenticate('github', { session: false }),
+  userController.gitHubCallback,
+);
 
 module.exports = router;
