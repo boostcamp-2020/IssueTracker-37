93469505b4c67d167977a552890322959c4bff59
songwonseok <dnjstjr0517@gmail.com>
2020-11-03 12:02:50 +0900

    Refactor #31 : utils bcrypt 사용
    - utils의 bcrypt 모듈 사용
    - user-repository 모듈 사용

--- server/src/services/user-service.ts
+++ server/src/services/user-service.ts
@@ -1,8 +1,5 @@
-const bcrypt = require('bcrypt');
-
-const { models } = require('../sequelize').default;
-
-const saltRounds: number = 10;
+import userRepo from '../repositories/user-repository';
+import { encryptPassword } from '../utils/bcrypt';
 
 interface signupForm {
   email: string;
@@ -10,24 +7,12 @@ interface signupForm {
   name: string;
 }
 
-const getHash = (password: string) => {
-  return new Promise((resolve: any, reject: any) => {
-    bcrypt.genSalt(saltRounds, (err: any, salt: any) => {
-      if (err) reject(err);
-      bcrypt.hash(password, salt, (Err: any, hash: string) => {
-        if (Err) reject(Err);
-        resolve(hash);
-      });
-    });
-  });
-};
-
 class UserService {
   createUser = async (payload: signupForm) => {
     try {
-      payload.password = String(await getHash(payload.password));
+      payload.password = encryptPassword(payload.password);
 
-      const insertUser = await models.user.create(payload);
+      const insertUser = await userRepo.insert(payload);
 
       return insertUser;
     } catch (err) {
