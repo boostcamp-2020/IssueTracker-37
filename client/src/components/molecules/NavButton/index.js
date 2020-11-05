02ded64db64be3a196ba7d09c1aa79a7afff6d4a
sumniy <sumniy94@gmail.com>
2020-11-05 17:23:10 +0900

    Feat #80 : molecules 요소 NavButton 구현
    - ImgTitleCount 요소를 재사용하여 NavButton 작성
    - 클릭 시 각각 라벨 페이지, 마일스톤 페이지로 이동하도록 구현

---
+++ client/src/components/molecules/NavButton/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+
+import labelImg from '@img/label.png';
+import milestoneImg from '@img/milestone.png';
+import cn from 'classnames';
+
+import { StyledNavButton } from './style';
+import ImgTitleCount from '../ImgTitleCount';
+
+const NavButton = (props) => {
+  const { labelCount, milestoneCount, className } = props;
+  const history = useHistory();
+
+  return (
+    <StyledNavButton className={cn(className)}>
+      <ImgTitleCount
+        src={labelImg}
+        alt="라벨 아이콘"
+        count={labelCount}
+        className="label"
+        onClick={() => history.push('/label')}
+      >
+        Labels
+      </ImgTitleCount>
+      <ImgTitleCount
+        src={milestoneImg}
+        alt="마일스톤 아이콘"
+        count={milestoneCount}
+        onClick={() => history.push('/milestone')}
+      >
+        Milestones
+      </ImgTitleCount>
+    </StyledNavButton>
+  );
+};
+
+ImgTitleCount.defaultProps = {};
+
+ImgTitleCount.propTypes = {
+  labelCount: PropTypes.number,
+  milestoneCount: PropTypes.number,
+  className: PropTypes.string,
+};
+
+export default NavButton;
