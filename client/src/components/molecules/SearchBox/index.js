7f4d918077a96400e44486b8a3c594a5118f4862
sumniy <sumniy94@gmail.com>
2020-11-05 17:26:15 +0900

    Feat #80 : SearchBox 추가 구현
    - Dropdown 컴포넌트를 포함하도록 구현
    - isFilter state를 사용하여 조건부 렌더링 구현

--- client/src/components/molecules/SearchBox/index.js
+++ client/src/components/molecules/SearchBox/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import cn from 'classnames';
 
 import Input from '@atoms/Input';
 import Button from '@atoms/Button';
+import Dropdown from '@molecules/Dropdown';
 
 import { StyledSearchBox } from './style';
 
@@ -15,21 +17,38 @@ const SearchBox = (props) => {
     id,
     afterContent,
     onChange,
-    onClick,
+    // onClick,
     children,
+    className,
   } = props;
 
   const searchbar = 'searchbox';
+  const [isFilter, setIsFilter] = useState(false);
+
+  const onClick1 = () => {
+    setIsFilter(!isFilter);
+  };
+
+  const items = [
+    'Open issues',
+    'Your issues',
+    'Everything assigned to you',
+    'Everything mentioning you',
+    'Closed issues',
+  ];
+
+  const title = 'Filter issues';
 
   return (
-    <StyledSearchBox>
+    <StyledSearchBox className={cn(className)}>
       <Button
-        onClick={onClick}
+        onClick={onClick1}
         afterContent={afterContent}
         className={searchbar}
       >
         {children}
       </Button>
+      {isFilter ? <Dropdown title={title} items={items}></Dropdown> : ``}
       <Input
         type={type}
         value={value}
@@ -55,6 +74,7 @@ SearchBox.propTypes = {
   children: PropTypes.string,
   onChange: PropTypes.func,
   onClick: PropTypes.func,
+  className: PropTypes.string,
 };
 
 export default SearchBox;
