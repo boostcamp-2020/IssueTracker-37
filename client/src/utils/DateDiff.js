e3fd86de478de5f793685d9316d1289d1a7693b3
hoo00nn <hoo00nn@naver.com>
2020-11-05 21:24:16 +0900

    Feat #noissue : 날짜 계산 유틸 함수 추가
    - 현재 시간과 파라미터로 받은 시간을 비교하여 시간 차이를 구하는 함수 추가

---
+++ client/src/utils/DateDiff.js
@@ -0,0 +1,23 @@
+export const getDateDiff = (date) => {
+  const dateObj = new Date();
+  const year = dateObj.getFullYear();
+  const month = dateObj.getMonth() + 1;
+  const day = dateObj.getDate();
+  const hours = dateObj.getHours();
+  const minutes = dateObj.getMinutes();
+  const seconds = dateObj.getSeconds();
+
+  const currentDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  const diff =
+    (new Date(currentDate).getTime() - new Date(date).getTime()) / 1000;
+
+  if (diff < 60) return `${diff} seconds ago`;
+  if (diff < 60 * 60) return `${Math.floor(diff / 60)} minutes`;
+  if (diff < 60 * 60 * 60) return `${Math.floor(diff / (60 * 60))} hours`;
+  if (diff < 60 * 60 * 60 * 24)
+    return `${Math.floor(diff / (60 * 60 * 24))} days`;
+  if (diff < 60 * 60 * 24 * 30 * 12)
+    return `${Math.floor(diff / (60 * 60 * 24 * 30))} months`;
+  if (diff > 60 * 60 * 24 * 30 * 12)
+    return `${Math.floor(diff / (60 * 60 * 24 * 30 * 12))} years`;
+};
