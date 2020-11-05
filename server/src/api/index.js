d2a96cbbe1256f723e69090b84049502db603acc
hoo00nn <hoo00nn@naver.com>
2020-11-05 11:02:20 +0900

    Feat #noissue : api auth 미들웨어 등록
    - api 요청 시 유효한 토큰이 들어왔는지 인증하는 미들웨어 등록

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const userRouter = require('@api/user/user-routes');
 const issueRouter = require('@api/issue/issue-routes');
+const { apiAuth } = require('@middlewares/auth');
 
 const router = express.Router();
 
 router.use('/user', userRouter);
+
+router.use(apiAuth);
 
 router.use('/issue', issueRouter);
 
