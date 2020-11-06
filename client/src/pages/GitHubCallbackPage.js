9bcf0cc3bca0e1f198d90f55f21ab57971d5d3f7
hoo00nn <hoo00nn@naver.com>
2020-11-06 10:28:57 +0900

    Fix #noissue : 토큰 뽑아내는 정규식 변경
    - csrftoken 때문에 제대로 뽑아내지 못함
    - 정규식 변경해서 해결

--- client/src/pages/GitHubCallbackPage.js
+++ client/src/pages/GitHubCallbackPage.js
@@ -1,17 +1,18 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { setToken } from '@utils/token';
 
 const deleteCookie = (name) => {
-  document.cookie = name + '=; expires=Thu, 01 Jan 1999 00:00:10 GMT;';
+  document.cookie = `${name}=; expires=Thu, 01 Jan 1999 00:00:10 GMT;`;
 };
 
 const GitHubCallbackPage = () => {
   const history = useHistory();
 
   useEffect(() => {
-    const token = document.cookie.match(/(?<=(token=)).*/)[0];
+    const token = document.cookie.match(/(?<=([^(csrtf)]token=))().*/)[0];
 
-    localStorage.setItem('authorization', token);
+    setToken(token);
     deleteCookie('token');
 
     history.push('/');
