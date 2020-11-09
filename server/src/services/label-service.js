6f55a595c66f0d4606e23aa530c5eb0716b3812c
hoo00nn <hoo00nn@naver.com>
2020-11-09 11:33:38 +0900

    Feat #86 : getLabels 서비스 함수 구현
    - model에서 라벨리스트를 불러오고 라벨의 총 개수를 데이터에 추가하여 리턴하도록 구현

--- server/src/services/label-service.js
+++ server/src/services/label-service.js
@@ -1,6 +1,16 @@
 const labelModel = require('@models/label-model');
 
-class LabelService { }
+class LabelService {
+  async getLabels() {
+    const labels = await labelModel.getLabels();
+    const data = {
+      count: labels.length,
+      labels,
+    };
+
+    return data;
+  }
+}
 
 const labelService = new LabelService();
 
