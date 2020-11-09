96c4e9aa3f21ca7dd5c63cdcd3a17b2617dd072e
songwonseok <dnjstjr0517@gmail.com>
2020-11-09 11:32:48 +0900

    Feat #85 : 마일스톤 조회 service 구현
    - 마일스톤에 포함되는 issue들의 상태를 조회하기 위해 인수로 issue model을 보냄

--- server/src/services/milestone-service.js
+++ server/src/services/milestone-service.js
@@ -1,6 +1,18 @@
 const milestoneModel = require('@models/milestone-model');
+const issueModel = require('@models/issue-model')
 
-class MilestoneService { }
+class MilestoneService {
+  async getMilestones() {
+    try {
+      const milestones = await milestoneModel.getMilestones(issueModel);
+
+    } catch (err) {
+      throw new Error();
+    }
+
+    return milestones
+  }
+}
 
 const milestoneService = new MilestoneService();
 
