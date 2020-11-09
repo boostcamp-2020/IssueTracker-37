fc69d2de0085c8ec9167d4f242417a4f142c2e83
songwonseok <dnjstjr0517@gmail.com>
2020-11-09 12:22:31 +0900

    Feat #92 : 라벨 수정 service 구현
    - result[0]의 값이 1이면 수정, 0이면 수정되지 않은 경우이므로 0일 때 에러 처리

--- server/src/services/label-service.js
+++ server/src/services/label-service.js
@@ -1,6 +1,17 @@
 const labelModel = require('@models/label-model');
 
-class LabelService { }
+class LabelService {
+  async updateLabel(payload) {
+    try {
+      const result = await labelModel.updateLabel(payload);
+
+      if (!result[0]) throw new Error();
+
+    } catch (err) {
+      throw new Error();
+    }
+  }
+}
 
 const labelService = new LabelService();
 
