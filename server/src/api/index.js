5db2fb2d3f9d4f4ab5689821e8e5cc932c367e81
hoo00nn <hoo00nn@naver.com>
2020-11-09 11:31:22 +0900

    Feat #86 : label router 연결
    - /api/label uri 로 들어오는 요청 라우터와 연결

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userRouter = require('@api/user/user-routes');
 const issueRouter = require('@api/issue/issue-routes');
+const labelRouter = require('@api/label/label-routes');
 const { apiAuth } = require('@middlewares/auth');
 
 const router = express.Router();
@@ -10,5 +11,6 @@ router.use('/user', userRouter);
 router.use(apiAuth);
 
 router.use('/issue', issueRouter);
+router.use('/label', labelRouter);
 
 module.exports = router;
