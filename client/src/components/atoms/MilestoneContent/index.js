62e72a2fc56a53e24825e954026e080a9f166ea1
hoo00nn <hoo00nn@naver.com>
2020-11-10 19:28:13 +0900

    Feat #96 : MilestoneContent atom 요소 구현
    - 마일스톤이 없을 경우 default로 설정한 내용 표시
    - 마일스톤이 있을 경우 선택된 마일스톤 정보 표시

---
+++ client/src/components/atoms/MilestoneContent/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Span from '@atoms/Span';
+import ProgressBar from '@atoms/ProgressBar';
+
+import {
+  StyledMilestoneContent,
+  StyledContentWrapper,
+  StyledContentBottom,
+} from './style';
+
+const MilestoneContent = ({ checkList, percent }) => {
+  return (
+    <StyledMilestoneContent>
+      {checkList.map((list) => (
+        <StyledContentWrapper key={list.id}>
+          <ProgressBar percent={percent}></ProgressBar>
+          <StyledContentBottom>
+            <Span spanType="SMALL">{list.title}</Span>
+          </StyledContentBottom>
+        </StyledContentWrapper>
+      ))}
+    </StyledMilestoneContent>
+  );
+};
+
+MilestoneContent.defaultProps = {};
+
+MilestoneContent.propTypes = {
+  checkList: PropTypes.array,
+  percent: PropTypes.string,
+};
+
+export default MilestoneContent;
