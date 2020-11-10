59e5c795f96fb6a2d417a023218232a769e49d04
hoo00nn <hoo00nn@naver.com>
2020-11-10 19:28:51 +0900

    Feat #96 : AssigneeContent atom 요소 구현
    - 선택된 Assignee가 없을 경우 default로 설정한 내용 표시
    - 선택된 Assignee가 있을 경우 선택된 Assignee 정보 표시

---
+++ client/src/components/atoms/AssigneeContent/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const StyledAssigneeContent = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const StyledContentWrapper = styled.div`
+  display: flex;
+  margin-bottom: 5px;
+
+  & > img {
+    margin-right: 10px;
+  }
+`;
