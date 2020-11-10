e54affb7482ada500d35a7a5633f997165a8b465
songwonseok <dnjstjr0517@gmail.com>
2020-11-10 11:08:09 +0900

    Refactor #noissue : getData() 구현
    - 반복되는 request 코드를 줄이기 위해 getData() 메소드 구현

--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -5,28 +5,49 @@ import Navbar from '@organisms/Navbar';
 import IssueContent from '@organisms/IssueContent';
 import Template from './Template';
 
+const getData = async (resourceName, setData) => {
+  const {
+    data: { data },
+  } = await request.get({
+    uri: `/${resourceName}`,
+  });
+
+  setData(data);
+};
+
 const MainPage = () => {
   // header
   // Navbar => users  labels.length, milestone.length;
   // IssueContent => issues, users, labels, milestones;
-  const [issues, setIssue] = useState([]);
+  const [issues, setIssues] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [milestones, setMilestones] = useState([]);
+  const [labels, setLabels] = useState([]);
 
   useEffect(() => {
-    (async () => {
-      const {
-        data: { data },
-      } = await request.get({
-        uri: '/issue',
-      });
-
-      setIssue(data);
-    })();
+    getData('label', setLabels);
+    getData('issue', setIssues);
+    getData('milestone', setMilestones);
+    getData('user', setUsers);
   }, []);
+
   return (
     <Template
       Header={<Header></Header>}
-      Navbar={<Navbar labelCount={1} milestoneCount={1}></Navbar>}
-      IssueContent={<IssueContent issues={issues}></IssueContent>}
+      Navbar={
+        <Navbar
+          labelCount={labels.length}
+          milestoneCount={milestones.length}
+        ></Navbar>
+      }
+      IssueContent={
+        <IssueContent
+          issues={issues}
+          milestones={milestones}
+          labels={labels}
+          users={users}
+        ></IssueContent>
+      }
     ></Template>
   );
 };
