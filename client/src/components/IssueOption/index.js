30500f0a8ca13416db7112ea85821ae5488ccd1f
hoo00nn <hoo00nn@naver.com>
2020-11-10 19:32:27 +0900

    Feat #96 : IssueOption 컴포넌트 구현
    - assignee, label, mileston 별 타입에 따라 해당하는 view 랜더링 되도록 구현

--- client/src/components/IssueOption/index.js
+++ client/src/components/IssueOption/index.js
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import Span from '@atoms/Span';
 import DropdownButton from '@molecules/DropdownButton';
+import AssigneeContent from '@atoms/AssigneeContent';
+import LabelContent from '@atoms/LabelContent';
+import MilestoneContent from '@atoms/MilestoneContent';
 
 import { StyledIssueOption, StyledIssueOptionBottom } from './style';
 
 const IssueOption = ({ IssueOptionProps }) => {
+  const [defaultContent, setDefalutCotent] = useState('');
+  const [renderComponent, setRenderComponent] = useState([]);
+
+  useEffect(() => {
+    if (IssueOptionProps.title === 'Assignees') {
+      setDefalutCotent('No one-assign yourself');
+      setRenderComponent(
+        <AssigneeContent checkList={IssueOptionProps.items}></AssigneeContent>,
+      );
+    }
+
+    if (IssueOptionProps.title === 'label') {
+      setDefalutCotent('None yet');
+      setRenderComponent(
+        <LabelContent checkList={IssueOptionProps.items}></LabelContent>,
+      );
+    }
+
+    if (IssueOptionProps.title === 'milestone') {
+      setRenderComponent(
+        <MilestoneContent
+          checkList={IssueOptionProps.items}
+          percent="30"
+        ></MilestoneContent>,
+      );
+    }
+  }, []);
+
   return (
     <>
       <StyledIssueOption>
         <DropdownButton {...IssueOptionProps}></DropdownButton>
         <StyledIssueOptionBottom>
-          <Span color="GRAY" spanType="SMALL">
-            No one-assign yourself
-          </Span>
+          {IssueOptionProps.checkList.length === 0 ? (
+            <Span color="GRAY" spanType="SMALL">
+              {defaultContent}
+            </Span>
+          ) : (
+              renderComponent
+            )}
         </StyledIssueOptionBottom>
       </StyledIssueOption>
     </>
