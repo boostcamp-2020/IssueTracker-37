fab790d173d33fcf3c73bdbda4ed551817a3a755
songwonseok <dnjstjr0517@gmail.com>
2020-11-11 21:10:36 +0900

    Feat #noissue : Header onClick 이벤트 추가
    - Header에 main페이지로 이동하는 onClick 이벤트 추가

--- client/src/components/organisms/Header/index.js
+++ client/src/components/organisms/Header/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
-import ImgTitle from '@atoms/ImgTitle';
+import ImgTitleCount from '@molecules/ImgTitleCount';
 import Button from '@atoms/Button';
 import { removeToken } from '@utils/token';
 import { StyledHeader } from './style';
 
 const Header = () => {
   const history = useHistory();
+
+  const onClickHeader = () => {
+    history.push('/');
+  };
+
   const onClickLogout = () => {
     removeToken();
     history.push('/signin');
@@ -16,13 +21,14 @@ const Header = () => {
   return (
     <>
       <StyledHeader>
-        <ImgTitle
-          styleType="header"
-          src="https://avatars3.githubusercontent.com/u/52775389?s=60&v=4"
-          alt="프로필 기본 이미지"
+        <ImgTitleCount
+          color="white"
+          SVGName="ISSUE"
+          onClick={onClickHeader}
+          displayCount="none"
         >
           ISSUE
-        </ImgTitle>
+        </ImgTitleCount>
         <Button onClick={onClickLogout}>로그아웃</Button>
       </StyledHeader>
     </>
