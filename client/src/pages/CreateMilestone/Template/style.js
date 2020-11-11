f068ddc41c9363d96d8155ccaed7a0fec0432258
songwonseok <dnjstjr0517@gmail.com>
2020-11-11 21:07:31 +0900

    Feat #107 : CreateMilestone Page, Template 구현
    - SimpleNavbar 대신 SubHeader 컴포넌트 사용
    - EditMilestone 페이지와 비슷함

---
+++ client/src/pages/CreateMilestone/Template/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const StyledCreateMilestoneTemplate = styled.div`
+  & > main {
+    padding: 0 80px;
+  }
+  & > main > header {
+    margin-top: 30px;
+  }
+
+  & > main > section {
+    margin-top: 20px;
+  }
+`;
