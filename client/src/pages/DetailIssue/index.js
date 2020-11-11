98f741486b6703372e4bf6140850e8f0f8c24cb3
kimhakjun <dkd7142@naver.com>
2020-11-11 22:11:34 +0900

    Feat #105 : 이슈상세 페이지 로직작성
    - 이슈타이틀 수정 api 로직 작성
    - edit 작성창 open, close 하는 이벤트 연결

--- client/src/pages/DetailIssue/index.js
+++ client/src/pages/DetailIssue/index.js
@@ -1,16 +1,71 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
+import request from '@lib/axios';
 import Header from '@organisms/Header';
+import useFetch from '@hoc/useFetch';
 import DetailIssueHeader from '@organisms/DetailIssueHeader';
 import { StyledDetailIssueWrapper } from './style';
+import useInput from '../../hooks/useInput';
 
 const DetailIssue = () => {
-  useEffect(() => {});
+  const [issue, setIssue, loading] = useFetch(location.pathname);
+  const [isEdited, setIsEdited] = useState(false);
+  const [isActived, setIsActived] = useState(true);
+  const [title, onChangeTitle, setTitle] = useInput();
+
+  useEffect(() => {
+    title?.length === 0 ? setIsActived(false) : setIsActived(true);
+  }, [title]);
+
+  useEffect(() => {
+    if(!loading){
+      setTitle(issue.title);
+    }
+  }, [issue]);
+
+  const EventHandler = {
+    onOpenEdit: () => {
+      setIsEdited(true);
+    },
+    onCloseEdit: () => {
+      setIsEdited(false);
+    },
+
+    onClickSave: async () => {
+      const { data } = await request.put({uri : location.pathname, data: {
+        title
+      }});
+
+      if (data.state === 'success'){
+        setIssue({...issue, title});
+        setIsEdited(false);
+      }
+      alert(data.message);
+    },
+    onChangeTitle,
+  };
+
+  const DetailIssueHeaderProps = {
+    issue,
+    isEdited,
+    isActived,
+    titleInputs: {
+      name: 'title',
+      value: title,
+    },
+  };
 
   return (
     <>
       <Header></Header>
       <StyledDetailIssueWrapper>
-        <DetailIssueHeader></DetailIssueHeader>
+        {loading ? (
+          <div>loading...</div>
+        ) : (
+          <DetailIssueHeader
+            DetailIssueHeaderProps={DetailIssueHeaderProps}
+            {...EventHandler}
+          ></DetailIssueHeader>
+        )}
       </StyledDetailIssueWrapper>
     </>
   );
