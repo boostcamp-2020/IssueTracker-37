80c014db59d152f80654033f32b00ba56e59c7ba
songwonseok <dnjstjr0517@gmail.com>
2020-11-11 12:44:48 +0900

    Feat #100 : MilestonItem 구현
    - MilestoneContent에서 사용되는 Molecule 구현
    - isState에 따라 changeButton을 변경 true면 Close, false면 Open
    - Edit 버튼을 누르면 마일스톤 수정 페이지로 이동
    - Close or Open 버튼을 누르면 마일스톤의 상태를 변경
    - Delete 버튼을 누르면 마일스톤 삭제

---
+++ client/src/components/molecules/MilestoneItem/index.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+import cn from 'classnames';
+
+import ImgTitleCount from '@molecules/ImgTitleCount';
+import ProgressBar from '@molecules/ProgressBar';
+import Span from '@atoms/Span';
+import { StyledMilestoneItem } from './style';
+
+const monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+const getDueDate = (dueDate) => {
+  const date = new Date(dueDate);
+
+  return `Due by ${monthNames[date.getMonth()]
+    } ${date.getDate()}, ${date.getFullYear()}`;
+};
+
+const getRate = (milestone) => {
+  const total = milestone.Issues.length;
+  const open = milestone.Issues.filter((issue) => issue.state === true).length;
+  const close = total - open;
+  const rate = total > 0 ? (close / total) * 100 : 0;
+
+  return {
+    rate: Math.round(rate),
+    open: String(open),
+    close: String(close),
+  };
+};
+const MilestoneItem = (props) => {
+  const {
+    milestone,
+    isState,
+    className,
+    onDelete,
+    updateMilestoneState,
+  } = props;
+  const history = useHistory();
+  const issueState = getRate(milestone);
+  const changeButton = isState ? 'Close' : 'Open';
+
+  return (
+    <StyledMilestoneItem className={cn(className)}>
+      <div>
+        <div className="title">
+          <Span fontWeight="bold">{milestone.title}</Span>
+        </div>
+        <ImgTitleCount
+          SVGName={'CALENDAR'}
+          spanType="SMALL"
+          displayCount="none"
+          color="GRAY"
+          className="dueDate"
+        >
+          {getDueDate(milestone.due_date)}
+        </ImgTitleCount>
+        <div className="description">
+          <Span spanType="SMALL" color="GRAY">
+            {milestone.description}
+          </Span>
+        </div>
+      </div>
+
+      <div>
+        <div className="rate">
+          <ProgressBar percent={issueState.rate} />
+        </div>
+
+        <div className="states">
+          <Span>{`${issueState.rate}% complete`}</Span>
+          <Span>{`${issueState.open} opend`}</Span>
+          <Span>{`${issueState.close} closed`}</Span>
+        </div>
+        <div className="buttons">
+          <Span
+            color="blue"
+            onClick={() => history.push(`/milestone/edit/${milestone.id}`)}
+          >
+            Edit
+          </Span>
+          <Span color="blue" onClick={() => updateMilestoneState(milestone)}>
+            {changeButton}
+          </Span>
+          <Span color="red" onClick={() => onDelete(milestone.id)}>
+            Delete
+          </Span>
+        </div>
+      </div>
+    </StyledMilestoneItem>
+  );
+};
+
+MilestoneItem.defaultProps = {
+  onDelete: () => { },
+};
+
+MilestoneItem.propTypes = {
+  milestone: PropTypes.object,
+  isState: PropTypes.bool,
+  className: PropTypes.string,
+  onDelete: PropTypes.func,
+  updateMilestoneState: PropTypes.func,
+};
+
+export default MilestoneItem;
