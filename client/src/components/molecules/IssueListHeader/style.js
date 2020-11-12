bce682be301d3758921742816cca2c45417edee8
songwonseok <dnjstjr0517@gmail.com>
2020-11-13 04:45:10 +0900

    Feat #114 #115 : IssueListHeader 추가기능 구현
    - checkbox의 checked 유무에 따라 다른 버튼을 보여주도록 변경
    - DropDownButton 리팩토링 적용

--- client/src/components/molecules/IssueListHeader/style.js
+++ client/src/components/molecules/IssueListHeader/style.js
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
-export const StyledIssueListHeader = styled.div`
+export const StyledIssueListHeader = styled.form`
   display: flex;
   width: 100%;
   background-color: #fafbfc;
   height: 55px;
   border-bottom: 1px solid #e1e4e8;
   float: right;
-  padding: 10px;
+  padding: 10px 0 10px 10px;
 
   & > input {
     display: block;
@@ -32,9 +32,32 @@ export const StyledIssueListHeader = styled.div`
   & > .container .dropDownButtonContainer {
     display: flex;
     flex: 10 0 0;
+    padding: 6px 0;
   }
 
   & > .container .dropDownButtonContainer .dropDownBtn {
     flex: 2 0 0;
+    outline: none;
+    display: flex;
+    justify-content: center;
+    margin: auto;
+  }
+
+  & .dropdown-header {
+    justify-content: flex-end;
+    cursor: pointer;
+  }
+
+  & .main-drowdown-menu {
+    width: 250px;
+  }
+
+  & .mark-as-button {
+    flex: 2 0 0;
+    outline: none;
+    display: flex;
+    margin: auto;
+    justify-content: flex-end;
+    padding-right: 40px;
   }
 `;
