1fc55c8184ae153b28e4b1d880af0de459705f7b
kimhakjun <dkd7142@naver.com>
2020-11-12 13:33:08 +0900

    Feat #105 : 이슈 생성 페이지 수정
    - 이슈 생성페이지에 사용되는 파라미터 이슈폼에 전달하여 재사용

--- client/src/pages/CreateIssue/style.js
+++ client/src/pages/CreateIssue/style.js
@@ -2,18 +2,15 @@ import styled from 'styled-components';
 
 export const StyledWrapper = styled.div`
   display: flex;
-  border: 1px solid blue;
+  padding-left: 25px;
   height: 100%;
   margin-top: 50px;
 `;
 
 export const StyledLeftContent = styled.div`
   flex: 7 0 0;
-  border: 1px solid green;
 `;
 
 export const StyledRightContent = styled.div`
   flex: 3 0 0;
-  padding-top: 40px;
-  border: 1px solid red;
 `;
