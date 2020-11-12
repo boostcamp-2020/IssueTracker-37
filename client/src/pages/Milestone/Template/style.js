211eb0642decdc9e07820885acc0e0169f034336
songwonseok <dnjstjr0517@gmail.com>
2020-11-12 15:51:57 +0900

    Style #100 : 페이지 margin-bottom 추가
    - 페이지 하단에 margin 40px 추가

--- client/src/pages/Milestone/Template/style.js
+++ client/src/pages/Milestone/Template/style.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const StyledMilestoneTemplate = styled.div`
   & > main {
     padding: 0 80px;
+    margin-bottom: 40px;
   }
   & > main > nav {
     margin-top: 40px;
