1c26f20628efc1d935ed8ecab42766d6ff0f4d58
songwonseok <dnjstjr0517@gmail.com>
2020-11-12 16:02:31 +0900

    Refactor #97 :  LabelItem, LabelContent 렌더링 방식 수정
    - 기존에 LabelItem에서 LabelForm을 관리한 것을 분리하여 LabelContent에서 렌더링 선택하도록 변경

--- client/src/components/molecules/LabelItem/index.js
+++ client/src/components/molecules/LabelItem/index.js
@@ -4,51 +4,27 @@ import PropTypes from 'prop-types';
 import cn from 'classnames';
 import IssueLabel from '@atoms/IssueLabel';
 import Span from '@atoms/Span';
-import LabelForm from '@molecules/LabelForm';
 
 import { StyledLabelItem } from './style';
 
 const LabelItem = (props) => {
-  const {
-    label,
-    className,
-    onEdit,
-    onDelete,
-    isForm,
-    onClose,
-    onSubmit,
-    isDuplicate,
-  } = props;
+  const { label, className, onEdit, onDelete } = props;
 
   return (
-    <>
-      {isForm !== label.id ? (
-        <StyledLabelItem className={cn(className)}>
-          <div className="item-label">
-            <IssueLabel className="label" labelColor={label.color}>
-              {label.title}
-            </IssueLabel>
-          </div>
-          <div className="item-description">
-            <Span>{label.description}</Span>
-          </div>
-          <div className="item-buttons">
-            <Span onClick={onEdit}>Edit</Span>
-            <Span onClick={onDelete}>Delete</Span>
-          </div>
-        </StyledLabelItem>
-      ) : (
-          <LabelForm
-            label={label}
-            onCloseForm={onClose}
-            onDelete={onDelete}
-            submitName="Save changes"
-            onSubmit={onSubmit}
-            formType="edit"
-            isDuplicate={isDuplicate}
-          ></LabelForm>
-        )}
-    </>
+    <StyledLabelItem className={cn(className)}>
+      <div className="item-label">
+        <IssueLabel className="label" labelColor={label.color}>
+          {label.title}
+        </IssueLabel>
+      </div>
+      <div className="item-description">
+        <Span>{label.description}</Span>
+      </div>
+      <div className="item-buttons">
+        <Span onClick={onEdit}>Edit</Span>
+        <Span onClick={onDelete}>Delete</Span>
+      </div>
+    </StyledLabelItem>
   );
 };
 
@@ -62,10 +38,7 @@ LabelItem.propTypes = {
   className: PropTypes.string,
   onEdit: PropTypes.func,
   onDelete: PropTypes.func,
-  onClose: PropTypes.func,
   isForm: PropTypes.number,
-  onSubmit: PropTypes.func,
-  isDuplicate: PropTypes.func,
 };
 
 export default LabelItem;
--- client/src/components/organisms/LabelContent/index.js
+++ client/src/components/organisms/LabelContent/index.js
@@ -4,6 +4,7 @@ import cn from 'classnames';
 
 import Span from '@atoms/Span';
 import LabelItem from '@molecules/LabelItem';
+import LabelForm from '@molecules/LabelForm';
 import { StyledLabelContent, StyledLabelListHeader } from './style';
 
 const LabelContent = (props) => {
@@ -20,25 +21,35 @@ const LabelContent = (props) => {
 
   const labelHeader = `${labels.length} Labels`;
 
+  const componentSelector = (label) => {
+    return label.id !== isForm ? (
+      <LabelItem
+        key={label.id}
+        label={label}
+        onDelete={() => onDelete(label.id)}
+        onEdit={() => onOpenForm(label.id)}
+      ></LabelItem>
+    ) : (
+        <LabelForm
+          key={label.id}
+          label={label}
+          onCloseForm={onCloseForm}
+          onDelete={onDelete}
+          submitName="Save changes"
+          onSubmit={onSubmit}
+          formType="edit"
+          isDuplicate={isDuplicate}
+        ></LabelForm>
+      );
+  };
+
   return (
     <StyledLabelContent className={cn(className)}>
       <StyledLabelListHeader>
         <Span>{labelHeader}</Span>
       </StyledLabelListHeader>
 
-      {labels &&
-        labels.map((label) => (
-          <LabelItem
-            key={label.id}
-            label={label}
-            isForm={isForm}
-            onDelete={() => onDelete(label.id)}
-            onEdit={() => onOpenForm(label.id)}
-            onClose={onCloseForm}
-            onSubmit={onSubmit}
-            isDuplicate={isDuplicate}
-          ></LabelItem>
-        ))}
+      {labels && labels.map((label) => componentSelector(label))}
     </StyledLabelContent>
   );
 };
