04411bd5015ba0c6f906df4a08aa3331f721a50f
songwonseok <dnjstjr0517@gmail.com>
2020-11-13 04:46:14 +0900

    Feat #114 : SearchBox onKeyPressHandler 구현
    - 필터링 조건을 입력하고 Enter를 누르면 onFilter 함수가 실행되는 이벤트 구현

--- client/src/components/molecules/SearchBox/index.js
+++ client/src/components/molecules/SearchBox/index.js
@@ -1,54 +1,54 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
 
 import Input from '@atoms/Input';
-import Button from '@atoms/Button';
-import Dropdown from '@molecules/Dropdown';
+import DropdownButton from '@molecules/DropdownButton';
 
 import { StyledSearchBox } from './style';
 
 const SearchBox = (props) => {
   const {
-    type,
-    value,
-    name,
+    inputValue,
     placeholder,
-    id,
     afterContent,
-    onChange,
     options,
-    children,
+    onFilter,
     className,
+    onChange,
+    onClick,
   } = props;
 
   const searchbar = 'searchbox';
-  const [isFilter, setIsFilter] = useState(false);
 
-  const onClick = () => {
-    setIsFilter(!isFilter);
+  const onKeyPressHandler = (e) => {
+    if (e.key === 'Enter') {
+      onFilter(inputValue);
+    }
   };
-
-  const title = 'Filter issues';
 
   return (
     <StyledSearchBox className={cn(className)}>
-      <Button
-        onClick={onClick}
+      <DropdownButton
+        className="search-dropdown"
+        buttonName="Label"
+        title="Filters"
+        dropdownHeader="Filter Issues"
+        spanType="DEFAULT"
+        color="BLACK"
+        items={options}
         afterContent={afterContent}
-        className={searchbar}
-      >
-        {children}
-      </Button>
-      {isFilter ? <Dropdown title={title} items={options}></Dropdown> : ``}
+        onClick={onClick}
+        dropdownType="searchBox"
+      ></DropdownButton>
+
       <Input
-        type={type}
-        value={value}
-        name={name}
+        type="text"
+        value={inputValue}
         placeholder={placeholder}
-        id={id}
-        onChange={onChange}
+        onKeyPress={onKeyPressHandler}
         className={searchbar}
+        onChange={onChange}
       />
     </StyledSearchBox>
   );
@@ -58,15 +58,16 @@ SearchBox.defaultProps = {};
 
 SearchBox.propTypes = {
   type: PropTypes.string,
-  value: PropTypes.string,
+  inputValue: PropTypes.string,
   name: PropTypes.string,
   placeholder: PropTypes.string,
   id: PropTypes.string,
   afterContent: PropTypes.string,
-  children: PropTypes.string,
   onChange: PropTypes.func,
   className: PropTypes.string,
   options: PropTypes.array,
+  onFilter: PropTypes.func,
+  onClick: PropTypes.func,
 };
 
 export default SearchBox;
