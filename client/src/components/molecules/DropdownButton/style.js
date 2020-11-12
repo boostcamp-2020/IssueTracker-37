5805d592b33beee772b7ed90cc0fc666dda9d6ec
songwonseok <dnjstjr0517@gmail.com>
2020-11-13 04:41:43 +0900

    Refactor #115 : DropdownButton typeHandler 구현
    - type에 따라 다른 속성을 사용하기 위해 typeHandler 함수 구현

--- client/src/components/molecules/DropdownButton/style.js
+++ client/src/components/molecules/DropdownButton/style.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const StyledDropdownButton = styled.div`
+  
   position: relative;
   justify-content: center;
   align-items: center;
@@ -19,9 +20,12 @@ export const StyledDropdownButton = styled.div`
     color: #606770;
     outline: none;
     padding: 0;
+
 `;
 
 export const StyledDropdownHeader = styled.div`
+  width: fit-content;
+  outline: none;
   display: flex;
   justify-content: space-between;
   margin-bottom: 10px;
@@ -33,23 +37,29 @@ export const StyledDropdownHeader = styled.div`
     & > svg > path {
       fill: #0366d6;
     }
+  }
+
+  & svg {
+    display: none;
+  }
 `;
 
 export const StyledDropdownMenu = styled.div`
   position: absolute;
-  top: 30px;
+  top: -5px;
   right: 0;
-  width: 100%;
-  border: 1px solid #fafbfc;
+  width: 200px;
+  border: 1px solid #e1e4e8;
   border-radius: 6px;
   z-index: 1;
-
+  box-shadow: 2px 3px 2px lightgray;
   & > span.dropdown-menu-header {
+    padding: 8px 30px;
+    cursor: unset;
     display: block;
     border-top-left-radius: 6px;
     border-top-right-radius: 6px;
-    padding: 8px 10px;
-    background-color: white;
+    background-color: #fafbfc;
     color: #24292e;
   }
 `;
