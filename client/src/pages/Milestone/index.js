493329f17f77e22ea70b85d73e450d3b88a0b428
songwonseok <dnjstjr0517@gmail.com>
2020-11-13 04:50:20 +0900

    Style #noissue : Milestone Page useEffect 삭제
    - useFetch로 useEffect를 사용하지 않아서 삭제

--- client/src/pages/Milestone/index.js
+++ client/src/pages/Milestone/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import request from '@lib/axios';
 import useFetch from '@hooks/useFetch';
