91fce105225d8dd3e4e76f2910e146fca3541f4c
songwonseok <dnjstjr0517@gmail.com>
2020-11-13 04:51:14 +0900

    Feat #114 #115 : filter, bulkUpdate 기능 구현
    - filter 관련 함수들 구현
    - checkBox 관련 함수 구현

--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -1,35 +1,149 @@
 import React, { useEffect, useState } from 'react';
 import request from '@lib/axios';
+import { parser, filter } from '@utils/filter';
 import Header from '@organisms/Header';
 import Navbar from '@organisms/Navbar';
 import IssueContent from '@organisms/IssueContent';
+import useFetch from '@hooks/useFetch';
 import Template from './Template';
 
-const getData = async (resourceName, setData) => {
-  const {
-    data: { data },
-  } = await request.get({
-    uri: `/${resourceName}`,
-  });
+const sortOptionsConverter = {
+  Newest: 'created-desc',
+  Oldest: 'created-asc',
+  'Most commented': 'comments-desc ',
+  'Least commented': 'comments-asc',
+  'Recently updated': 'updated-desc',
+  'Least recently updated': 'updated-asc',
+};
 
-  setData(data);
+const searchBoxOptionsConverter = {
+  'Open issues': { type: 'is', value: 'open' },
+  'Closed issues': { type: 'is', value: 'closed' },
+  'Your issues': { type: 'author', value: '내이름' },
+  'Everything assigned to you': { type: 'assignee', value: '내이름' },
+};
+
+const initialFilterOptions = {
+  titles: [],
+  options: {},
+};
+
+const filterOptionsToString = (filterOptions) => {
+  let text = `${filterOptions.titles.join(' ')}`;
+
+  for (const [key, value] of Object.entries(filterOptions.options)) {
+    text += ` ${key}:${value}`;
+  }
+
+  return text;
 };
 
 const MainPage = () => {
-  // header
-  // Navbar => users  labels.length, milestone.length;
-  // IssueContent => issues, users, labels, milestones;
-  const [issues, setIssues] = useState([]);
-  const [users, setUsers] = useState([]);
-  const [milestones, setMilestones] = useState([]);
-  const [labels, setLabels] = useState([]);
+  const [filterOptions, setFilterOptions] = useState(initialFilterOptions);
+  const [issues, setIssues] = useFetch({
+    uri: '/issue',
+    initialData: [],
+    option: { name: 'checked', value: false },
+  });
+  const [users] = useFetch({ uri: '/user', initialData: [] });
+  const [milestones] = useFetch({
+    uri: '/milestone',
+    initialData: [],
+  });
+  const [labels] = useFetch({ uri: '/label', initialData: [] });
+  const [filteredIssues, setFilteredIssues] = useState([]);
+  const [inputValue, setInputValue] = useState('');
 
   useEffect(() => {
-    getData('label', setLabels);
-    getData('issue', setIssues);
-    getData('milestone', setMilestones);
-    getData('user', setUsers);
-  }, []);
+    setInputValue(filterOptionsToString(filterOptions));
+    setFilteredIssues(filter(issues, filterOptions));
+  }, [filterOptions]);
+
+  useEffect(() => {
+    setFilteredIssues([...issues]);
+  }, [issues]);
+
+  const onFilter = (input) => {
+    const options = parser(input);
+
+    setFilterOptions(options);
+  };
+
+  const onBulkUpdate = async (value) => {
+    const state = value === 'Open';
+
+    const checkedList = filteredIssues.reduce((acc, issue) => {
+      if (issue.checked) {
+        acc.push(issue.id);
+      }
+      return acc;
+    }, []);
+
+    try {
+      await request.put({
+        uri: '/issue',
+        data: { issues: checkedList, state },
+      });
+      const {
+        data: { data },
+      } = await request.get({ uri: 'issue' });
+
+      setIssues(data.map((v) => ({ ...v, checked: false })));
+    } catch (err) {
+      alert('업데이트를 실패했습니다.');
+    }
+  };
+
+  const dropdownEventHandler = (type, value) => {
+    if (type !== 'markAs') {
+      const temp = { ...filterOptions };
+      let tempValue = value;
+
+      if (tempValue.includes(' ')) {
+        tempValue = `"${tempValue}"`;
+      }
+
+      if (type === 'searchBox') {
+        const searchOption = searchBoxOptionsConverter[value];
+
+        temp.options = {};
+        temp.options[searchOption.type] = searchOption.value;
+      } else {
+        temp.options[type] =
+          type === 'sort' ? sortOptionsConverter[tempValue] : tempValue;
+      }
+
+      setFilterOptions(temp);
+    } else {
+      onBulkUpdate(value);
+    }
+  };
+
+  const onChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const onCheckBoxChange = (e) => {
+    const id = Number(e.target.value);
+
+    const isChecked = e.target.checked;
+    let temp = [];
+
+    if (id === 0) {
+      temp = filteredIssues.map((issue) => {
+        const newIssue = { ...issue };
+
+        newIssue.checked = isChecked;
+        return newIssue;
+      });
+    } else {
+      const index = filteredIssues.findIndex((issue) => issue.id === id);
+
+      temp = [...filteredIssues];
+      temp[index].checked = isChecked;
+    }
+    setFilteredIssues(temp);
+  };
 
   return (
     <Template
@@ -38,14 +152,20 @@ const MainPage = () => {
         <Navbar
           labelCount={labels.length}
           milestoneCount={milestones.length}
+          onFilter={onFilter}
+          inputValue={inputValue}
+          onChange={onChange}
+          onClick={dropdownEventHandler}
         ></Navbar>
       }
       IssueContent={
         <IssueContent
-          issues={issues}
+          issues={filteredIssues}
           milestones={milestones}
           labels={labels}
           users={users}
+          onClick={dropdownEventHandler}
+          onCheckBoxChange={onCheckBoxChange}
         ></IssueContent>
       }
     ></Template>
